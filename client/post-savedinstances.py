7d84a51d8dce132f97db3af86b9be0a10f4b1757
Claus Ruepp <claus.ruepp@gmail.com>
2022-07-31 16:39:43 +0200

    fix config parsing and posting

--- client/post-savedinstances.py
+++ client/post-savedinstances.py
@@ -14,27 +14,37 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.INFO)
 logger.addHandler(stdout_handler)
 
+file = 'SavedInstances.lua'
+server_url = ''
+file_path = ''
+
 config = configparser.ConfigParser()
+
 try:
     config.read('config.ini')
-    if not config['path_to_savedinstances_lua']:
+    if not config.get('config', 'path_to_savedinstances_lua'):
         if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
             abs_path = os.path.split(os.path.dirname(sys.executable))[0]
         else:
             abs_path = os.path.abspath('..')
-        file = 'SavedInstances.lua'
         file_path = os.path.join(abs_path, file)
-        logger.info('no path to savedinstances.lua provided, using default: ', file_path)
+        logger.info(
+            'no path to savedinstances.lua provided, using default: %s', file_path)
     else:
-        file_path = config['path_to_savedinstances_lua']
-        logger.info('path to savedinstances.lua: ', file_path)
-    if not config['server_url']:
+        file_path = config.get(
+            'config', 'path_to_savedinstances_lua')
+        logger.info('path to savedinstances.lua: %s', file_path)
+
+    if not os.path.exists(file_path):
+        raise Exception('savedinstances.lua path does not exist, exitting')
+
+    if not config.get('config', 'server_url'):
         raise Exception('no server_url provided')
+    else:
+        server_url = config.get('config', 'server_url')
+        logger.info('server url: %s', server_url)
 except Exception as e:
     logger.error(e)
-
-if not os.path.exists(file_path):
-    logger.error('savedinstances.lua path does not exist, exitting')
     exit(1)
 
 
@@ -56,7 +66,7 @@ post_file()
 
 file_observer = watchdog.observers.Observer()
 file_observer.schedule(PostingFileSystemEventHandler(),
-                       abs_path, recursive=False)
+                       file_path, recursive=False)
 file_observer.start()
 
 try:
