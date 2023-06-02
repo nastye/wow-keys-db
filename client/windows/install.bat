c2184f7acb7d6b1a7b5cbf79241d37f14d657c9a
Claus Ruepp <claus.ruepp@gmail.com>
2023-06-03 00:27:45 +0200

    config and windows specific changes

--- client/windows/install.bat
+++ client/windows/install.bat
@@ -1,4 +1,4 @@
 SET SCRIPT_PATH=%~dp0
-schtasks /Create /TN nastye-post-savedinstances /TR "%SCRIPT_PATH%post-savedinstances-noconsole.exe" /SC ONLOGON /F
+schtasks /Create /TN nastye-post-savedinstances /TR "%SCRIPT_PATH%..\post-savedinstances-noconsole.exe" /SC ONLOGON /F
 schtasks /Run /TN nastye-post-savedinstances
 pause
